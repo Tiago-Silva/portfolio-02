55924088084e0a5b220bc4529049b20857d0ad63
Tiago <ti.almeida58@gmail.com>
2024-11-05 13:33:19 -0300

    feat: creating typed script

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -9,3 +9,24 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
+
+const typed = new Typed('.multiple-text',  {
+    strings: [
+        'Desenvolvimento em Java',
+        'Front-end com React',
+        'Front-end com Next.js',
+        'Aplicativos em React Native',
+        'HTML e CSS Profissionais',
+        'JavaScript Dinâmico',
+        'Backend com Node.js',
+        'Interfaces com Design Responsivo',
+        'Clean Code',
+        'Clean Architecture',
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+});
+
+
