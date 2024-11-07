1dc8e012c7d98aaed53b5fe3e1e5c66dfea33fb4
Tiago <ti.almeida58@gmail.com>
2024-11-07 09:02:39 -0300

    feat: using library swiper for slide pagination

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -29,4 +29,29 @@ const typed = new Typed('.multiple-text',  {
     loop: true,
 });
 
+var swiper = new Swiper(".project-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
+
 
